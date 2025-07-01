7b02b0eb9112957be54451255d38ee4644b607ab
Sakura-I0 <dcm2297@gmail.com>
2025-07-01 15:20:12 -0500

    Initial commit

---
+++ src/client/java/com/anki/enchants/AnkiEnchantsClient.java
@@ -0,0 +1,10 @@
+package com.anki.enchants;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnkiEnchantsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/anki/enchants/AnkiEnchantsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.anki.enchants;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AnkiEnchantsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
